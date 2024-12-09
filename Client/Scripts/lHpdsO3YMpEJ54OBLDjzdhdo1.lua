7ea6f75e22a7e5f75028e7b50a9ac79bbd469c2e
Lorvernyr <loxerya@gmail.com>
2024-12-09 21:13:23 +0700

    Update and rename Sentinal.sph to lHpdsO3YMpEJ54OBLDjzdhdo1.lua

---
+++ Client/Scripts/lHpdsO3YMpEJ54OBLDjzdhdo1.lua
@@ -0,0 +1,50 @@
+local S3R3N1TY_T4BL3 = {
+
+    -- @ title for set in ui library
+    ["TITLE"] = {
+        ["LEFT"] = "seraph - script's";
+        ["RIGHT"] = "development version";
+
+        -- // Set Name in LocalScripts
+        ["SCRIPTS"] = "seraph.scripts";
+    };
+
+    -- @ color Theme for use as main
+    ["COLOR"] = {
+        ["MAIN"] = {
+            ["1"] = Color3.fromRGB(173, 95, 251);
+            ["2"] = Color3.fromRGB(73, 0, 147);
+        };
+
+        -- // Other Themes
+        ["AP0C4LYP2E"] = {
+            ["1"] = Color3.fromRGB(255,0,127);
+            ["2"] = Color3.fromRGB(58,0,29);
+        };
+        ["LUXUR4C"] = {
+            ["1"] = Color3.fromRGB(254, 192, 92);
+            ["2"] = Color3.fromRGB(132, 96, 48);
+        };
+        ["S3R4PH"] = {
+            ["1"] = Color3.fromRGB(173, 95, 251);
+            ["2"] = Color3.fromRGB(138,43,226);
+        };
+    };
+
+    -- @ set online status for checking
+    ["ONLINE"] = true;
+
+    -- @ set left bottom icon
+    ["ASSET ICONIC"] = "130516554311911";
+
+    -- @ set default icon for profile
+    ["DEFAULT ICONIC"] = "1056216310373154837";
+
+    -- @ set notify states
+    ["NOTIFICATION"] = true;
+
+    -- @ set draggble states
+    ["DRAGGABLE"] = false;
+}
+
+return S3R3N1TY_T4BL3
