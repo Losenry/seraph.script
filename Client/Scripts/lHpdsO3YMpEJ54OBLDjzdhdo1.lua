6e289ac01c1689d7a9a8c8a3a9db2d3aa7a1b445
Lorvernyr <loxerya@gmail.com>
2024-12-17 19:05:07 +0700

    Update lHpdsO3YMpEJ54OBLDjzdhdo1.lua

--- Client/Scripts/lHpdsO3YMpEJ54OBLDjzdhdo1.lua
+++ Client/Scripts/lHpdsO3YMpEJ54OBLDjzdhdo1.lua
@@ -39,7 +39,7 @@ local S3R3N1TY_T4BL3 = {
     ["ONLINE"] = true;
 
     -- @ set left bottom icon
-    ["ASSET ICONIC"] = "6031754553";
+    ["ASSET ICONIC"] = "13271381622";
 
     -- @ set default icon for profile
     ["DEFAULT ICONIC"] = "1056216310373154837";
