e7a52352b6303ecec3e168de3e1dcfc57c4d5ee9
Lorvernyr <loxerya@gmail.com>
2024-12-18 21:43:37 +0700

    Create QuestLib.lua

---
+++ Client/Scripts/QuestLib.lua
@@ -0,0 +1,53 @@
+local AllQuest = {
+    {1,"Bandit Quest Giver"};
+    {10,"Adventurer"};
+    {30,"Pirate Adventurer"}; -- 3
+    {60,"Desert Adventurer"};
+    {90,"Villager"};
+    {120,"Marine"}; -- 6
+    {150,"Sky Adventurer"};
+	{190,"Jail Keeper"};
+    {250,"Colosseum Quest GIver"};
+    {300,"The Mayor"}; -- 9
+    {375,"King Neptune"};
+    {450,"Mole"};
+    {525,"Gan Fall Adventurer"}; -- 12
+    {625,"Freezeburg Quest Giver"};
+    
+    -- V Second Sea V --
+    
+    {700,"Area 1 Quest Giver"};
+    {775,"Area 2 Quest Giver"}; -- 15
+    {875,"Marine Quest Giver"};
+    {950,"Graveyard Quest Giver"};
+    {1000,"Snow Quest Giver"}; -- 18
+    {1100,"Ice Quest Giver"};
+    {1175,"Fire Quest Giver"};
+    {1250,"Rear Crew Quest Giver"}; -- 21
+    {1300,"Front Crew Quest Giver"};
+    {1350,"Frost Quest Giver"};
+    {1425,"Forgotten Quest Giver"}; -- 24
+    
+    -- V Third Sea V --
+
+    {1500,"Pirate Port Quest Giver"};
+    {1575,"Amazon Quest Giver"};
+    {1625,"Amazon Area 2 Quest Giver"}; -- 27
+    {1700,"Marine Tree Quest Giver"};
+    {1775,"Turtle Adventure Quest Giver"};
+    {1825,"Deep Forest Quest Giver"}; -- 30
+    {1900,"Deep Forest Area 2 Quest Giver"};
+	{1975,"Haunted Castle Quest Giver 1"};
+    {2025,"Haunted Castle Quest Giver 2"};
+	{2075,"Peanut Quest Giver"};
+	{2125,"Ice Cream Quest Giver"};
+	{2200,"Cake Quest Giver 1"};
+	{2250,"Cake Quest Giver 2"};
+    {2300,"Choc Quest Giver 1"};
+    {2350,"Choc Quest Giver 2"};
+    {2400,"Candy Quest Giver 1"};
+    {2450,"Tiki Quest Giver 1"};
+    {2500,"Tiki Quest Giver 2"};
+    {2550,"Tiki Quest Giver 3"};
+}
+return AllQuest
