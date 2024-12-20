4483a0f73eea273725971ffe124a7c71474d036d
Lorvernyr <loxerya@gmail.com>
2024-12-20 20:49:39 +0700

    Update Server.lua

--- Client/LocalScript/Init/Visualization/Server.lua
+++ Client/LocalScript/Init/Visualization/Server.lua
@@ -1,6 +1,6 @@
 local ServerFunc = {};
 local isPrivate = false;
-local loadstring = loadstring;
+local fask = task;
 
 spawn(function()
     pcall(function()
@@ -38,12 +38,7 @@ function ServerFunc:TeleportFast()
         function TPReturner()
             local Site
             if foundAnything == "" then
-                Site =
-                    game.HttpService:JSONDecode(
-                        game:HttpGet(
-                            "https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100"
-                        )
-                    )
+                Site = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100"))
             else
                 Site =
                     game.HttpService:JSONDecode(
@@ -114,11 +109,89 @@ function ServerFunc:TeleportFast()
     end
 end
 
+--/ Teleport Func
+fask = task
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local File = pcall(function()
+	AllIDs = game:GetService("HttpService"):JSONDecode(readfile("NotSameServers.json"))
+end)
+
+if not File then
+	table.insert(AllIDs, actualHour)
+	writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
+end
+
+function TPReturner()
+	local Site
+	if foundAnything == "" then
+		Site = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100"))
+	else
+		Site = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" ..PlaceID .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. foundAnything))
+	end
+	local ID = ""
+	if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+		foundAnything = Site.nextPageCursor
+	end
+	local num = 0
+	for i, v in pairs(Site.data) do
+		local Possible = true
+		ID = tostring(v.id)
+		if tonumber(v.maxPlayers) > tonumber(v.playing) then
+			for _, Existing in pairs(AllIDs) do
+				if num ~= 0 then
+					if ID == tostring(Existing) then
+						Possible = false
+					end
+				else
+					if tonumber(actualHour) ~= tonumber(Existing) then
+						local delFile = pcall(function()
+							delfile("NotSameServers.json")
+							AllIDs = {}
+							table.insert(AllIDs, actualHour)
+						end)
+					end
+				end
+				num = num + 1
+			end
+			if Possible == true then
+				table.insert(AllIDs, ID)
+				fask.wait()
+				pcall(function()
+					writefile("NotSameServers.json", game:GetService("HttpService"):JSONEncode(AllIDs))
+					fask.wait()
+					local args = {
+						[1] = "teleport",
+						[2] = ID
+					}
+
+					game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer(unpack(args))
+				end)
+				fask.wait(0.5)
+			end
+		end
+	end
+end
+
+function Teleport()
+	while fask.wait() do
+		pcall(function()
+			TPReturner()
+			if foundAnything ~= "" then
+				TPReturner()
+			end
+		end)
+	end
+end
+
 function ServerFunc:NormalTeleport()
     if isPrivate == false then
         task.delay(15,function()
             pcall(function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/Api/main/BitCoinDeCodeApi.cs"))()
+				Teleport()
             end)
         end)
         repeat task.wait()
