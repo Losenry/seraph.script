f8f24e710d02f66807a24969c541c8af3795ccea
Lorvernyr <loxerya@gmail.com>
2024-12-31 04:46:18 +0700

    Update Loader.lua

--- Client/LocalScript/Init/Serenity/LocalScript/Loader.lua
+++ Client/LocalScript/Init/Serenity/LocalScript/Loader.lua
@@ -1,3 +1,3 @@
 if not _G.Key or not _G.DiscordId then return end
-print(tostring(game.GameId))
+print(_G.Key, '|', _G.DiscordId, '|', game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
 return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Losenry/seraph.script/refs/heads/main/Client/LocalScript/Init/Serenity/Script/serenity.game.loader"))();
