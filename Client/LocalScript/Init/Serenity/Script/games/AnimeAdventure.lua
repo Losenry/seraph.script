ea3451a25609e47e9907609833c0839002c86803
Lorvernyr <loxerya@gmail.com>
2024-12-31 19:56:48 +0700

    Update AnimeAdventure.lua

--- Client/LocalScript/Init/Serenity/Script/games/AnimeAdventure.lua
+++ Client/LocalScript/Init/Serenity/Script/games/AnimeAdventure.lua
@@ -1,1 +1,5 @@
-
+if _G.Full_AutoPlay then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Init/Serenity/Script/games/AnimeAdventure/FullPlay.lua'))();
+else
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Init/Serenity/Script/games/AnimeAdventure/Normal.lua'))();
+end
